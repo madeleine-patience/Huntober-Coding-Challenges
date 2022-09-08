c8b2fa9ce3c44099ce204286c1e4a6f2b02ed562
madeleinebelanger <madeleinepbelanger@gmail.com>
2022-09-08 13:49:26 -0700

    update

--- Blaws Challenges/Week2 - Day 5.js
+++ Blaws Challenges/Week2 - Day 5.js
@@ -27,7 +27,7 @@ return chunkedArr
 let myArray = [[1,2,3],[4,5,6],[7,8,9]]
 console.log(shuffle(myArray))
 
-// researched arr
+// researched function
 
 const shuffle2=(myArray)=> {
     let size= myArray.length
