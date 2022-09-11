620312725b1392393aef6823ff5b97e7555c773f
madeleinebelanger <madeleinepbelanger@gmail.com>
2022-09-11 13:48:40 -0700

    update

--- Blaws Challenges/Week2 - Day 5.js
+++ Blaws Challenges/Week2 - Day 5.js
@@ -1,3 +1,5 @@
+//first try
+
 const shuffle=(myArray)=>{
 let size=myArray.length
 let arr= myArray.join().split(',')
@@ -11,8 +13,6 @@ while(indexAssignments.length<9){
 let scrambledArr=[]
 for(let i=0; i<length;i++){
 scrambledArr.push(arr[indexAssignments[i]])
-
-
 }
 scrambledArr
 
@@ -20,13 +20,24 @@ let chunkedArr=[]
 for (let i=0; i<scrambledArr.length; i+=size){
     const chunk = scrambledArr.slice(i,i+size)
     chunkedArr.push(chunk)
-
 }
 
 return chunkedArr
-
-
 }
-
-
+let myArray = [[1,2,3],[4,5,6],[7,8,9]]
 console.log(shuffle(myArray))
+
+// researched arr
+
+const shuffle2=(myArray)=> {
+    let size= myArray.length
+    let arr = myArray.join().split(',').sort(()=> Math.random()-0.5)
+
+    let finalArr=[]
+    for (let i=0; i<arr.length; i+=size){
+    const chunk = arr.slice(i,i+size)
+    finalArr.push(chunk)
+}
+    return finalArr
+}
+console.log(shuffle2(myArray))
