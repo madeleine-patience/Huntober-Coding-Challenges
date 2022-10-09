f43781ff44fc3d50003dbb788735604974240f2c
madeleinebelanger <madeleinepbelanger@gmail.com>
2022-10-08 19:09:03 -0700

    Saturday Update

--- Day 3.js
+++ Day 3.js
@@ -1,10 +1,10 @@
 
 function createSpace(message){
-let secretCode= ["s","p","a","c","e"]
+let secretCode= ["S","p","a","c","e"]
 for (let i =0; i<message.length; i++){
     message= message.split('').map(items=> items===secretCode[i] ? ' ':items).join('')
 }
 return message
 }
 
-console.log(createSpace('AyouEcould!thinkKthisAisYhard_toYreadKbeforeYreplacingEthe_keyYcharacters'))
+console.log(createSpace('e!!1gv)tSllt8cvbdeDHEdVw!OOt1#Aa.2MDuLWYpP^VVjDch1SOiv#ylhgmQfs'))
